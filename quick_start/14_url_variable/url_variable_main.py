7115662299de2ca3bd4e1e38e6ef94995f6050cc
jaru <jaru@moonrabbit.co>
2014-12-29 16:41:19 +0900

    + 14_url_variable @ quick_start

---
+++ quick_start/14_url_variable/url_variable_main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "User {}".format(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "Post {}".format(post_id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
