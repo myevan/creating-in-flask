1128e217363490849e96932dce613f60ecd3cef1
jaru <jaru@moonrabbit.co>
2014-12-30 10:41:31 +0900

    ! pep8 @ official_tutorial/02_application_setup

--- official_tutorial/02_application_setup/flaskr/flaskr_application_setup_main.py
+++ official_tutorial/02_application_setup/flaskr/flaskr_application_setup_main.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
