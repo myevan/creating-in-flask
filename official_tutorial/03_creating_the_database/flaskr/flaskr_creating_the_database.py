9bd82a0e880e9597a8fc7e779a022e90b27d191d
jaru <jaru@moonrabbit.co>
2014-12-30 10:52:37 +0900

    + 03_creating_the_database @ official_tutorial

---
+++ official_tutorial/03_creating_the_database/flaskr/flaskr_creating_the_database.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+from flask import Flask
+from contextlib import closing
+
+DEBUG = True
+DATABASE = '/tmp/flaskr.db'
+SECRET_KEY = 'DEVELOPMENT_KEY'
+USER_NAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+if __name__ == '__main__':
+    app.run()
